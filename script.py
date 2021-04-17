bffbfe68f8863e6b5a5ada189f2bb1feb05e556f
S. Abubakar Bin Qasim <65397022+SAbubakarQ@users.noreply.github.com>
2021-04-17 18:41:21 -0500

    Create script.py

---
+++ script.py
@@ -0,0 +1,17 @@
+#Bismillah - AbuabkarQ
+
+greeting = ("Welcome! This is the SABQ ChatBot v.I!");
+print(greeting);
+
+bot_1 = ("I will repeat what you say to me.");
+print (bot_1);
+
+stuff_to_echo = input("What would you like me to say?");
+print("You said: " + stuff_to_echo);
+
+bot_2 = ("Now, that's working we'll move on.");
+print(bot_2);
+
+name = input("What's your name, friend? ");
+print("Nice to meet you, " + name);
+
